01f1f22bde1edc1698b6a807592339be6644448d
Hassan Hashmi <hashmihassan47@gmail.com>
2025-09-21 11:48:50 -0400

    Initialize maze problem

---
+++ DP/maze/maze.py
@@ -0,0 +1,20 @@
+"""
+maze.py
+
+This module solves the below probelem
+
+Given an N x M grid, in how many ways can a rabbit get from the top-left to the 
+  bottom-right corner if it can only move left or down?
+
+Function:
+- `grid_paths`
+
+Author:
+  Hassan Hashmi
+"""
+
+def grid_paths() -> int:
+  """
+  Compute the number of paths to reach the bottom left of a n x m grid
+  """
+  return 0
